06c12b2081db0230e90fa06d735c381095f57077
Mingi15 <109404745+Mingi15@users.noreply.github.com>
2022-07-16 12:00:34 -0800

    var height, width ,color;
    // When size is submitted by the user, call makeGrid()
    $('#sizePicker').submit(function(event) {
        event.preventDefault();
        height = $('#inputHeight').val();
        width = $('#inputWidth').val();
        makeGrid( height,width)
        //console.log(`Height:${height} and width: ${width}`);
    })
    function makeGrid(x , y) {
        $("tr").remove();
    
     // Your code goes here!
    
      for (var i = 1; i <= x; i++) {
        $('#pixelCanvas').append('<tr id=table' + i + "></tr>")
    
        for (var j = 1; j <= y; j++) {
            $('#table' + i).append('<td></td>')
    
        }
     }
    
     //add color to cell
    $("td").click(function addColor() {
        color = $('#colorPicker').val(),
    
           $(this).remove('style')
           if ($(this).attr('style')) {
    
        } else {
            $(this).attr('style','background-color:' + color);
        }
       })
    }

--- designs.js
+++ designs.js
@@ -1,10 +1,40 @@
 // Select color input
 // Select size input
+var height, width ,color;
 
 // When size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function(event) {
+    event.preventDefault();
+    height = $('#inputHeight').val();
+    width = $('#inputWidth').val();
+    makeGrid( height,width)
+    //console.log(`Height:${height} and width: ${width}`);
+})
+function makeGrid(x , y) {
+    $("tr").remove();
 
-function makeGrid() {
+ // Your code goes here!
 
-// Your code goes here!
+  for (var i = 1; i <= x; i++) {
+    $('#pixelCanvas').append('<tr id=table' + i + "></tr>")
+   
+    for (var j = 1; j <= y; j++) {
+        $('#table' + i).append('<td></td>')
+     
+    
+    
+    }
+ }
 
+ //add color to cell
+$("td").click(function addColor() {
+    color = $('#colorPicker').val(),
+   
+       $(this).remove('style')
+       if ($(this).attr('style')) {
+        
+    } else {
+        $(this).attr('style','background-color:' + color);
+    } 
+   })
 }
